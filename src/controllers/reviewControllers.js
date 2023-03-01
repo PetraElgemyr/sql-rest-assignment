cf3c2b225a15b0db3e236c676f014ab384db6bfc
h4zan <h4zan@hotmail.com>
2023-03-01 12:55:24 +0100

    added get reviews user

--- src/controllers/reviewControllers.js
+++ src/controllers/reviewControllers.js
@@ -2,12 +2,31 @@ const { userRoles } = require("../constants/users");
 const { sequelize } = require("../database/config");
 const { UnauthorizedError, NotFoundError } = require("../utils/errors");
 const { QueryTypes } = require("sequelize");
+const { NOTFOUND } = require("sqlite3");
 
 
-
+/* 
 exports.getAllReviewsForStore = async (req, res) => {
 
+} */
 
+exports.getAllReviewsByUserId = async (req, res) => {
 
+const userId = req.params.userId
+const [reviews, metadata] = await sequelize.query('SELECT * FROM reviews r WHERE fk_users_id = $userId',
+{
+    bind:{userId},
+    type: QueryTypes.SELECT  
 
+})
+
+if (!reviews) {
+
+    throw new NotFoundError('We could not find any reviews by this user')
+    
 }
+return res.json(reviews)
+
+  
+}
+
