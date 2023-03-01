f9b2d6636f5750ce282158ee542980f4dddc15db
petraelgemyr <petra.elgemyr@gmail.com>
2023-03-01 14:25:55 +0100

    seedDB trying

--- src/database/seedDb.js
+++ src/database/seedDb.js
@@ -4,10 +4,10 @@ const seedStoresDb = async () => {
   try {
     // Drop tables if exist
 
-    await sequelize.query(` DROP TABLE IF EXISTS users;`);
-    await sequelize.query(`DROP TABLE IF EXISTS reviews;`);
-    await sequelize.query(`DROP TABLE IF EXISTS stores;`);
-    await sequelize.query(`DROP TABLE IF EXISTS citys;`);
+    // await sequelize.query(`DROP TABLE IF EXISTS citys;`);
+    // await sequelize.query(`DROP TABLE IF EXISTS users;`);
+    // await sequelize.query(`DROP TABLE IF EXISTS reviews;`);
+    // await sequelize.query(`DROP TABLE IF EXISTS stores;`);
 
     // Create users table
     await sequelize.query(`
@@ -15,22 +15,15 @@ const seedStoresDb = async () => {
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	email TEXT NOT NULL UNIQUE,
 	password TEXT NOT NULL,
-	is_admin BOOLEAN NOT NULL DEFAULT 0 CHECK (is_admin IN (0, 1)) 
-    ); 
+	is_admin BOOLEAN NOT NULL DEFAULT 0 CHECK (is_admin IN (0, 1))
      `);
 
-    // Create reviews table
     await sequelize.query(`
-    CREATE TABLE IF NOT EXISTS reviews (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        review_content TEXT NOT NULL,
-        rating INTEGER,
-        fk_stores_id INTEGER NOT NULL,
-        fk_users_id INTEGER NOT NULL, 
-        FOREIGN KEY(fk_stores_id) REFERENCES stores(id),
-        FOREIGN KEY(fk_users_id) REFERENCES users(id)
-    );
-      `);
+      CREATE TABLE IF NOT EXISTS citys (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    city_name TEXT NOT NULL,
+    )
+    `);
 
     // Create stores table
     await sequelize.query(`
@@ -42,27 +35,49 @@ const seedStoresDb = async () => {
         fk_users_id INTEGER NOT NULL,
         FOREIGN KEY(fk_citys_id) REFERENCES citys(id),
         FOREIGN KEY(fk_users_id) REFERENCES users(id)
-    );
+    )
       `);
 
+    // Create reviews table
     await sequelize.query(`
-      CREATE TABLE IF NOT EXISTS citys (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    city_name TEXT NOT NULL,
+    CREATE TABLE IF NOT EXISTS reviews (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        review_content TEXT NOT NULL,
+        rating INTEGER,
+        fk_stores_id INTEGER NOT NULL,
+        fk_users_id INTEGER NOT NULL, 
+        FOREIGN KEY(fk_stores_id) REFERENCES stores(id),
+        FOREIGN KEY(fk_users_id) REFERENCES users(id)
+    )
+      `);
+
+    await sequelize.query(
+      `INSERT INTO users (email, password, is_admin) VALUES 
+      ('testus@gmail.com','testar123', 1), 
+      ('ksenia.ivanova@gmail.com', 'password456',0), 
+      ('petra.elgemyr@gmail.com','password789',0), 
+      ('hazan@gmail.com','password123',0)`
     );
-    `);
 
-    let storesInsertQuery =
-      "INSERT INTO stores (store_name, address, fk_citys_id, fk_users_id) VALUES ('Ica Supermarket Alvikstorg', 'Gustavslundsvägen 22', 1, userid), ('Lidl Göteborg', 'Kungsgatan 16', 3, userid), ('Lidl Medborgarplatsen', 'Folkungagatan 51', 1, userid), ('Coop Hötorget', 'T-station Hötorget', 1, userid), ('ICA Supermarket Majorna','Karl Johansgatan 21', 3, userid), ('Stora Coop Stadion','Stadiongatan 24', 2, userid), ('Ica Supermarket Hansa',' Stora Nygatan', 2, userid), ('Hemköp Triangeln',' Södra Förstadsgatan 58',2 , userid), ('ICA Supermarket Olskroken',' Redbergsvägen 14', 3, userid), ('Coop Mölndalsvägen','Mölndalsvägen 1', 3, userid);";
-      
-    let usersInsertQuery ="INSERT INTO users(email, password, is_admin) VALUES ('testus@gmail.com','testar123', 'true'), ('ksenia.ivanova@gmail.com', 'password456','false'), ('petra.elgemyr@gmail.com','password789','false'), ('hazan@gmail.com','password123','false')";
+    await sequelize.query(
+      `INSERT INTO citys (city_name) VALUES ('Stockholm'), ('Malmö'), ('Göteborg')`
+    );
 
+    await sequelize.query(`INSERT INTO stores 
+    (store_name, address, fk_citys_id, fk_users_id) 
+    VALUES 
+    ('Ica Supermarket Alvikstorg', 'Gustavslundsvägen 22', (SELECT id FROM citys c WHERE city_name = 'Stockholm'), 5),
+    ('Lidl Göteborg', 'Kungsgatan 16', (SELECT id FROM citys c WHERE city_name = 'Göteborg'), 6),
+    ('Lidl Medborgarplatsen', 'Folkungagatan 51', (SELECT id FROM citys c WHERE city_name = 'Stockholm'), 5), 
+    ('Coop Hötorget', 'T-station Hötorget', (SELECT id FROM citys c WHERE city_name = 'Stockholm'), userid), 
+    ('ICA Supermarket Majorna','Karl Johansgatan 21', (SELECT id FROM citys c WHERE city_name = 'Göteborg'), 7),
+    ('Stora Coop Stadion','Stadiongatan 24', (SELECT id FROM citys c WHERE city_name = 'Malmö'), 5),
+    ('Ica Supermarket Hansa',' Stora Nygatan', (SELECT id FROM citys c WHERE city_name = 'Malmö'), 6),
+    ('Hemköp Triangeln',' Södra Förstadsgatan 58',(SELECT id FROM citys c WHERE city_name = 'Malmö') , 7), 
+    ('ICA Supermarket Olskroken',' Redbergsvägen 14', (SELECT id FROM citys c WHERE city_name = 'Göteborg'), 7),
+    ('Coop Mölndalsvägen','Mölndalsvägen 1', (SELECT id FROM citys c WHERE city_name = 'Göteborg'), 6)`);
 
-    /* await sequelize.query(storesInsertQuery, {
-      bind: { storesInsertQuery: storesInsertQuery },
-    }); */
-
-    console.log("Database successfully populated with data...");
+    console.log("Database successfully populated with data");
   } catch (error) {
     // Log eny eventual errors to Terminal
     console.error(error);
