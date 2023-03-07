ab1f08cb1645d34e007f11ad8d295c9dfa0b48bf
petraelgemyr <petra.elgemyr@gmail.com>
2023-03-07 10:14:53 +0100

    readme, seeddb fixed

---
+++ README.md
@@ -0,0 +1,23 @@
+# Grocery Store REST API SQL Database Group Assignment
+
+## Description
+
+The assignment was to create a rest API with a SQLite database. The goal was to create a review site for grocery stores, where you can see the stores, register, login and review the stores. The user should also be able to delete and update certain data.
+
+Node
+Express server
+
+### Preparations
+
+DbDiagram
+![Alt text]('./Assignment.png', "DbDiagram")
+
+### Users
+
+Email: testus@gmail.com, password: "testar123" (admin)
+Email: ksenia.ivanova@gmail.com, password: "password456" (user & owner)
+Email: petra.elgemyr@gmail.com, password: "password789" (user & owner)
+Email: hazan@gmail.com, password: "password123" (user & owner)
+Email: user1@gmail.com, password: "userPassword123" (user)
+Email: user2@gmail.com, password: "userPassword456" (user)
+Email: user3@gmail.com, password: "userPassword789" (user)
--- src/database/seedDb.js
+++ src/database/seedDb.js
@@ -5,10 +5,10 @@ const seedStoresDb = async () => {
   try {
     // Drop tables if exist
 
-    // await sequelize.query(`DROP TABLE IF EXISTS citys;`);
-    // await sequelize.query(`DROP TABLE IF EXISTS users;`);
-    // await sequelize.query(`DROP TABLE IF EXISTS reviews;`);
-    // await sequelize.query(`DROP TABLE IF EXISTS stores;`);
+    await sequelize.query(`DROP TABLE IF EXISTS citys;`);
+    await sequelize.query(`DROP TABLE IF EXISTS users;`);
+    await sequelize.query(`DROP TABLE IF EXISTS reviews;`);
+    await sequelize.query(`DROP TABLE IF EXISTS stores;`);
 
     // Create users table
     await sequelize.query(`
@@ -56,18 +56,27 @@ const seedStoresDb = async () => {
     const passwordKsenia = "password456";
     const passwordPetra = "password789";
     const passwordHazan = "password123";
+    const passwordUser1 = "userPassword123";
+    const passwordUser2 = "userPassword456";
+    const passwordUser3 = "userPassword789";
 
     const newPasswordAdmin = createHashedPassword(passwordAdmin);
     const newPasswordKsenia = createHashedPassword(passwordKsenia);
     const newPasswordPetra = createHashedPassword(passwordPetra);
     const newPasswordHazan = createHashedPassword(passwordHazan);
+    const newUserPassword1 = createHashedPassword(passwordUser1);
+    const newUserPassword2 = createHashedPassword(passwordUser2);
+    const newUserPassword3 = createHashedPassword(passwordUser3);
 
     await sequelize.query(
       `INSERT INTO users (email, password, is_admin) VALUES 
       ('testus@gmail.com',${newPasswordAdmin}, 1), 
       ('ksenia.ivanova@gmail.com',${newPasswordKsenia},0), 
       ('petra.elgemyr@gmail.com',${newPasswordPetra},0), 
-      ('hazan@gmail.com',${newPasswordHazan},0)`
+      ('hazan@gmail.com',${newPasswordHazan},0)
+      ('user1@gmail.com',${newUserPassword1},0)
+      ('user2@gmail.com',${newUserPassword2},0)
+      ('user3@gmail.com',${newUserPassword3},0)`
     );
 
     await sequelize.query(
@@ -75,18 +84,47 @@ const seedStoresDb = async () => {
     );
 
     await sequelize.query(`INSERT INTO stores 
-    (store_name, address, fk_citys_id, fk_users_id) 
+    (store_name, address, fk_citys_id, fk_users_id, description) 
     VALUES 
-    ('Ica Supermarket Alvikstorg', 'Gustavslundsvägen 22', (SELECT id FROM citys c WHERE city_name = 'Stockholm'), 5),
-    ('Lidl Göteborg', 'Kungsgatan 16', (SELECT id FROM citys c WHERE city_name = 'Göteborg'), 6),
-    ('Lidl Medborgarplatsen', 'Folkungagatan 51', (SELECT id FROM citys c WHERE city_name = 'Stockholm'), 5), 
-    ('Coop Hötorget', 'T-station Hötorget', (SELECT id FROM citys c WHERE city_name = 'Stockholm'), userid), 
-    ('ICA Supermarket Majorna','Karl Johansgatan 21', (SELECT id FROM citys c WHERE city_name = 'Göteborg'), 7),
-    ('Stora Coop Stadion','Stadiongatan 24', (SELECT id FROM citys c WHERE city_name = 'Malmö'), 5),
-    ('Ica Supermarket Hansa',' Stora Nygatan', (SELECT id FROM citys c WHERE city_name = 'Malmö'), 6),
-    ('Hemköp Triangeln',' Södra Förstadsgatan 58',(SELECT id FROM citys c WHERE city_name = 'Malmö') , 7), 
-    ('ICA Supermarket Olskroken',' Redbergsvägen 14', (SELECT id FROM citys c WHERE city_name = 'Göteborg'), 7),
-    ('Coop Mölndalsvägen','Mölndalsvägen 1', (SELECT id FROM citys c WHERE city_name = 'Göteborg'), 6)`);
+    ('Ica Supermarket Alvikstorg', 'Gustavslundsvägen 22', (SELECT id FROM citys c WHERE city_name = 'Stockholm'), (SELECT id FROM users WHERE email = 'hazan@gmail.com'), 'Vardag eller fest? Vi har det du behöver. I butiken fyller vi varje dag på med nya färskvaror, frukt och grönt, mejeriprodukter och annat gott som förgyller din matvardag. I vår charkavdelning hittar du ostar, salami och skinkor för picknick, mys eller fest. Vi ses i butiken!'),
+    ('Lidl Göteborg', 'Kungsgatan 16', (SELECT id FROM citys c WHERE city_name = 'Göteborg'), (SELECT id FROM users WHERE email = 'ksenia.ivanova@gmail.com'), 'Vi pressar ner priserna utan att kvalitén ska ta stryk. För oss är det viktigt att bidra till hållbar och ansvarsfull odling av råvaror. Därför satsar vi bland annat på vårt ekologiska sortiment som fortsätter att växa. Vi har idag närmare 200 egna ekologiska varor och det kommer bara bli fler. Välkommen in till oss!',
+    ('Lidl Medborgarplatsen', 'Folkungagatan 51', (SELECT id FROM citys c WHERE city_name = 'Stockholm'), (SELECT id FROM users WHERE email = 'hazan@gmail.com'),'En stor butik på liten yta mitt på Södermalm. Lidl Medborgarplatsen strävar efter att vara ett av de billigaste alternativen på marknaden utan att förlora kvalitet.'), 
+    ('Coop Hötorget', 'T-station Hötorget', (SELECT id FROM citys c WHERE city_name = 'Stockholm'),(SELECT id FROM users WHERE email = 'hazan@gmail.com'), 'Coop Hötorget har stort utbud av allt som kan behövas. Mitt i Stockholm ligger vi med fokus på att det ska vara enkelt att snabbt handla hos oss påväg hem från jobbet utan något krångel. Välkommen in hos oss! '), 
+    ('ICA Supermarket Majorna','Karl Johansgatan 21', (SELECT id FROM citys c WHERE city_name = 'Göteborg'), (SELECT id FROM users WHERE email = 'petra.elgemyr@gmail.com'), 'Ica är butiken där du kan handla mat av högsta kvalitet till bästa pris. Vi har flera hundra butiker i Sverige där svenska och ekologiska produkter samsas med utvalda utländska delikatesser.  '),
+    ('Stora Coop Stadion','Stadiongatan 24', (SELECT id FROM citys c WHERE city_name = 'Malmö'), (SELECT id FROM users WHERE email = 'hazan@gmail.com'),'Coop har alltid fokus på hållbar ekologisk mat till ett bra pris, utan att minska kvaliten.'),
+    ('Ica Supermarket Hansa',' Stora Nygatan', (SELECT id FROM citys c WHERE city_name = 'Malmö'), (SELECT id FROM users WHERE email = 'ksenia.ivanova@gmail.com'),'Ica Hansa, närbutik lagom nära och enkelt att hitta hit.'),
+    ('Hemköp Triangeln',' Södra Förstadsgatan 58',(SELECT id FROM citys c WHERE city_name = 'Malmö') , (SELECT id FROM users WHERE email = 'petra.elgemyr@gmail.com'),'Mitt i köpcentrumet ligger vi. Hjärtat av Malmö, välkommen hit!'), 
+    ('ICA Supermarket Olskroken',' Redbergsvägen 14', (SELECT id FROM citys c WHERE city_name = 'Göteborg'), (SELECT id FROM users WHERE email = 'petra.elgemyr@gmail.com'),'Ica Olskroken finns när du behöver och där du behöver. Vardag eller helg så har vi det du söker.'),
+    ('Coop Mölndalsvägen','Mölndalsvägen 1', (SELECT id FROM citys c WHERE city_name = 'Göteborg'), (SELECT id FROM users WHERE email = 'ksenia.ivanova@gmail.com'), 'En välsorterad butik med matbar och deli.')`);
+
+    await sequelize.query(`INSERT INTO reviews (review_content, rating, fk_stores_id, fk_users_id) VALUES 
+    ('Coop är en av de större matbutikerna och har ett stort utbud av matvaror. Jag gillar deras sortiment av ekologiska och hälsosamma produkter, och också deras fokus på hållbarhet.', 4, (SELECT id FROM stores WHERE store_name = 'Coop Hötorget'),(SELECT id FROM users WHERE email = 'petra.elgemyr@gmail.com')),
+    ('Coop: Jag har handlat på Coop i flera år och jag är alltid nöjd med deras utbud och kvalitet på matvaror.', 5, (SELECT id FROM stores WHERE store_name = 'Coop Hötorget'),(SELECT id FROM users WHERE email = 'user1@gmail.com')),
+    ('ICA Nära är en liten och personlig matbutik som erbjuder bra kvalitet på matvaror. Jag gillar att de också har ett utbud av svenska produkter.', 4, (SELECT id FROM stores WHERE store_name = 'Ica Supermarket Alvikstorg'),(SELECT id FROM users WHERE email = 'user2@gmail.com')),
+    ('För liten butik med dåligt utbud. Ok priser.', 2, (SELECT id FROM stores WHERE store_name = 'Ica Supermarket Alvikstorg'),(SELECT id FROM users WHERE email = 'user3@gmail.com')),
+    ('Billigt men en aningen liten butik', 3, (SELECT id FROM stores WHERE store_name = 'Lidl Göteborg'),(SELECT id FROM users WHERE email = 'user2@gmail.com')),
+    ('Bästa butiken att komplementhandla på! Så billig och fräsch frukt och grönt-avdelning', 5, (SELECT id FROM stores WHERE store_name = 'Lidl Göteborg'),(SELECT id FROM users WHERE email = 'user1@gmail.com')),
+    ('Bra med veckans tema! Superkul idé och varierande varor', 5, (SELECT id FROM stores WHERE store_name = 'Lidl Medborgarplatsen'),(SELECT id FROM users WHERE email = 'user3@gmail.com')),
+    ('Utplockat på söndagar', 1, (SELECT id FROM stores WHERE store_name = 'Lidl Medborgarplatsen'),(SELECT id FROM users WHERE email = 'user2@gmail.com')),
+    ('Nära till kollektivtrafiken så lätt att hitta dit. Okej priser!', 3, (SELECT id FROM stores WHERE store_name = 'ICA Supermarket Majorna'),(SELECT id FROM users WHERE email = 'user2@gmail.com')),
+    ('Bra alternativ på onlineshoppen men lite dyrt!', 4, (SELECT id FROM stores WHERE store_name = 'ICA Supermarket Majorna'),(SELECT id FROM users WHERE email = 'user1@gmail.com')),
+    ('Bra deli och charkdisk', 4, (SELECT id FROM stores WHERE store_name = 'Stora Coop Stadion'),(SELECT id FROM users WHERE email = 'user3@gmail.com')),
+    ('Dyrt för en pensionär!', 2, (SELECT id FROM stores WHERE store_name = 'Stora Coop Stadion'),(SELECT id FROM users WHERE email = 'user2@gmail.com')),
+    ('Okej, inget mer än så', 3, (SELECT id FROM stores WHERE store_name = 'Ica Supermarket Hansa'),(SELECT id FROM users WHERE email = 'user3@gmail.com')),
+    ('ÄLSKAR Hansa som ligger precis bredvid mig och billig!', 5, (SELECT id FROM stores WHERE store_name = 'Ica Supermarket Hansa'),(SELECT id FROM users WHERE email = 'user1@gmail.com')),
+    ('Höga priser som allt annat i köpcentrumet...', 2, (SELECT id FROM stores WHERE store_name = 'Hemköp Triangeln'),(SELECT id FROM users WHERE email = 'user2@gmail.com')),
+    ('Stor med jättebra utbud. Hittar alltid det jag behöver här!!', 5, (SELECT id FROM stores WHERE store_name = 'Hemköp Triangeln'),(SELECT id FROM users WHERE email = 'user3@gmail.com')),
+    ('Sååå trevlig personal, alltid schyssta', 5, (SELECT id FROM stores WHERE store_name = 'ICA Supermarket Olskroken' ),(SELECT id FROM users WHERE email = 'user3@gmail.com')),
+    ('Få kassor så det är alltid kö här tyvärr', 3, (SELECT id FROM stores WHERE store_name = 'ICA Supermarket Olskroken'),(SELECT id FROM users WHERE email = 'user1@gmail.com')),
+    ('Jag gillar deras veckans erbjudande!', 4, (SELECT id FROM stores WHERE store_name = 'Coop Mölndalsvägen'),(SELECT id FROM users WHERE email = 'user2@gmail.com')),
+    ('Jag uppskattar deras sortiment av färskvaror och deras lokala produkter', 4, (SELECT id FROM stores WHERE store_name = 'Coop Mölndalsvägen'),(SELECT id FROM users WHERE email = 'user3@gmail.com')),
+
+
+
+
+
+
+`);
 
     console.log("Database successfully populated with data");
   } catch (error) {
